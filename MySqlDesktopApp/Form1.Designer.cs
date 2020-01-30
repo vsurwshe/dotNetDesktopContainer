419fa2bdf21e5636143c27f8894c580521026cb7
Vishvanath Surwshe <vsurwshe@gmail.com>
2020-01-30 12:33:38 +0530

    0.0.0: Initial start the code

---
+++ MySqlDesktopApp/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace MySqlDesktopApp
+{
+    partial class Desktop
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.name = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.email = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.address = new System.Windows.Forms.TextBox();
+            this.saveButton = new System.Windows.Forms.Button();
+           
+            this.SuspendLayout();
+            // 
+            // name
+            // 
+            this.name.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.name.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.name.Location = new System.Drawing.Point(196, 62);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(214, 26);
+            this.name.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(38, 65);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(105, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Empolyee name";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(38, 111);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label2.Size = new System.Drawing.Size(104, 19);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Empolyee email";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // email
+            // 
+            this.email.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.email.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.email.Location = new System.Drawing.Point(196, 108);
+            this.email.Name = "email";
+            this.email.Size = new System.Drawing.Size(214, 26);
+            this.email.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(38, 157);
+            this.label3.Name = "label3";
+            this.label3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label3.Size = new System.Drawing.Size(123, 19);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Empolyee Address";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // address
+            // 
+            this.address.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.address.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.address.Location = new System.Drawing.Point(196, 154);
+            this.address.Multiline = true;
+            this.address.Name = "address";
+            this.address.Size = new System.Drawing.Size(214, 95);
+            this.address.TabIndex = 4;
+            // 
+            // saveButton
+            // 
+            this.saveButton.BackColor = System.Drawing.Color.Lime;
+            this.saveButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.saveButton.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveButton.Location = new System.Drawing.Point(51, 302);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(106, 45);
+            this.saveButton.TabIndex = 6;
+            this.saveButton.Text = "Submit Data";
+            this.saveButton.UseVisualStyleBackColor = false;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+        
+            // 
+            // Desktop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1083, 547);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.address);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.email);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.name);
+            this.Name = "Desktop";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "General Applications Informations";
+            this.Load += new System.EventHandler(this.Desktop_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox email;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox address;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.BindingSource empBindingSource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn emailDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn addressDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ MySqlDesktopApp/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//----------------
+using System.Data.SqlClient;
+
+namespace MySqlDesktopApp
+{
+    public partial class Desktop : Form
+    {
+      
+
+        public Desktop()
+        {
+            InitializeComponent();
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            ////This is makeing connections btween app and databasse.(making road app and database)
+            ////SqlConnection con = new SqlConnection(Properties.Settings.Default.mysqlConn);
+            ////This is Command for executing query (Vahical)
+            //SqlCommand cmd; 
+            //try
+            //{  
+                
+      
+        
+            //    //this line open database connections means start the database connections.
+               
+            //    string query = "INSERT INTO `emp`(`name`, `email`, `address`) VALUES (@value2,@value3,@value4)";
+            //    cmd = new SqlCommand(query, con);
+            //    cmd.Parameters.AddWithValue("@value2",name.Text);
+            //    cmd.Parameters.AddWithValue("@value3",email.Text);
+            //    cmd.Parameters.AddWithValue("@value4",address.ToString());
+            //    con.Open();
+            //    int res = cmd.ExecuteNonQuery();
+            //    MessageBox.Show("Your Database Inserted Successfully " + res);
+
+            //}
+            //catch (Exception msg)
+            //{
+            //    MessageBox.Show(msg.Message);
+            //}
+            //finally
+            //{
+            //    //this line close database connections for new query executions.
+            //    con.Close();
+            //}
+        }
+
+        //This method called when applcations loads
+        private void Desktop_Load(object sender, EventArgs e)
+        {
+       
+
+        }
+
+     
+    }
+}
