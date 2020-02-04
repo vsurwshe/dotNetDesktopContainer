fed58557f30c5d83639e98c4d33323857364e8ca
unknown <Vishvanath Surwshe>
2020-02-04 14:38:42 +0530

    0.3.6: Added the user token in setting and geting

--- HotelProjectDesktopApp/UiScreen/UserDashboard.cs
+++ HotelProjectDesktopApp/UiScreen/UserDashboard.cs
@@ -19,7 +19,9 @@ namespace HotelDashboard.UiScreen
         private void UserDashboard_Load(object sender, EventArgs e)
         {
            HotelDashboard.WpfClient.Models.TokenResponse user = new HotelDashboard.WpfClient.Operations.ApiOperations().AuthenticateUser("v@v.com", "123456");
-           l2.Text = user.userToken;
+           Properties.Settings.Default.userToken = user.userToken;
+           Properties.Settings.Default.refreshToekn = user.userRefreshToken;
+           l2.Text = "User Token "+Properties.Settings.Default.userToken;
         }
     }
 }
