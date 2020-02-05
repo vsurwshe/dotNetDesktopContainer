98d69a00b3aedc4643346016a027c8fb3bcf7d50
Vishvanath Surwshe <vsurwshe@gmail.com>
2020-02-05 20:27:49 +0530

    0.3.12: Implemented the Regsiter Post API Call

--- HotelProjectDesktopApp/WpfClient/Operations/ApiOperations.cs
+++ HotelProjectDesktopApp/WpfClient/Operations/ApiOperations.cs
@@ -59,5 +59,56 @@ namespace HotelDashboard.WpfClient.Operations
                 return token;
             }
         }
+
+         /*
+         * This method register the user by taking User Model as parameter
+         */
+
+        public User registerUser(User userInput)
+        {   // This lines making josn data for api calling
+            string userBodyData = JsonConvert.SerializeObject(new {
+                name=userInput.name,
+                userEmail = userInput.userEmail,
+                userPassword = userInput.userPassword,
+            });
+            
+            // This line calling api using access token and store result of api into res varilabel
+            string res=this.callApiUsingAccessToken("auth/user/register","POST",userBodyData);
+            if (res != null)
+            {   // this line convert string result into Josn and paste into User Model and returning user model
+                return JsonConvert.DeserializeObject<User>(res);
+            }
+            return null;
+        }
+
+        // this method common for all api with access token needs to be call 
+        public string callApiUsingAccessToken(string userUrl,string userMethod, string userBody)
+        {   // this line makes final url for the api call using user url and base url.
+            string endpoint = this.baseUrl + userUrl;
+            string response=null;
+            // this condtions checking user token is there or not
+            if (Properties.Settings.Default.userToken != "")
+            {   // this line creating new object for WebClient call
+                WebClient wc = new WebClient();
+                // this line setting content type of request
+                wc.Headers["Content-Type"] = "application/json";
+                // this line adding Authoriazations with our user token
+                wc.Headers["Authorization"] = ""+Properties.Settings.Default.userToken;
+                try
+                {   //this checking which method want to execute the user Ex. POST and PUT
+                    if (userMethod == "POST" || userMethod == "PUT")
+                    {
+                        response = wc.UploadString(endpoint, userMethod, userBody);
+                    }else{ // this executing for the GET and DELETE method
+                        response = wc.UploadString(endpoint, userMethod);
+                    }
+                }
+                catch (WebException msg)
+                {
+                    return msg.ToString();
+                }
+            }
+            return response;
+        }
     }
 }
